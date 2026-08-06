97aff8b174fe95064b390f30da07093b33de3c0b
Htoo Aye Lwin <htooayelwinict@gmail.com>
2026-08-07 02:20:33 +0630

    chore: release 2.4.1

--- travis/coding_agent/config.py
+++ travis/coding_agent/config.py
@@ -19,7 +19,7 @@ try:
     VERSION = metadata.version(PACKAGE_NAME)
 except metadata.PackageNotFoundError:
     # Source checkouts can be imported before the editable package is installed.
-    VERSION = "2.4.0"
+    VERSION = "2.4.1"
 
 
 def expand_tilde_path(path: str) -> str:
