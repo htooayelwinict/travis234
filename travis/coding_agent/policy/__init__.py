4eabb1da63589130be34900c6fe435785ad72e3d
Htoo Aye Lwin <htooayelwinict@gmail.com>
2026-08-15 23:25:28 +0630

    feat(policy): add session-scoped authorization engine

--- travis/coding_agent/policy/__init__.py
+++ travis/coding_agent/policy/__init__.py
@@ -10,14 +10,27 @@ from travis.coding_agent.policy.types import (
     ToolPolicyMode,
     ToolPolicySettings,
 )
+from travis.coding_agent.policy.approval import (
+    ApprovalResponse,
+    SessionGrantSet,
+    ToolApprovalBroker,
+    ToolApprovalRequest,
+)
+from travis.coding_agent.policy.engine import ToolPolicyEngine, argument_fingerprint
 
 __all__ = [
     "ALL_TOOL_EFFECTS",
+    "ApprovalResponse",
+    "SessionGrantSet",
     "TOOL_EFFECT_ORDER",
     "TOOL_POLICY_MODE_ORDER",
     "TOOL_POLICY_REASON_CODES",
     "ToolEffect",
+    "ToolApprovalBroker",
+    "ToolApprovalRequest",
+    "ToolPolicyEngine",
     "ToolPolicyDecision",
     "ToolPolicyMode",
     "ToolPolicySettings",
+    "argument_fingerprint",
 ]
